5813eb895355397bbaa2459031012fb6471f7888
Viryasov Andrey <andreykaaaa73@gmail.com>
2025-04-08 12:29:40 +0300

    Viryasov

---
+++ src/main/java/org/example/MainClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+
+        System.out.println("1111Ура, мое первое выполнение кода на Java");
+        System.out.println("Ппц как долго компилируется код на моем ноуте");
+        System.out.println("Я уже работал с IDE, на Pycharm работал, знаю шо это такое:)");
+    }
+}
